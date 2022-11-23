5812dd63b45994a43bab74710f652c342ac6a40d
MateuszTwardy <mateusz.w.twardy@gmail.com>
2022-11-23 23:47:29 +0000

    Fixed problems, added getStaticProps

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,14 +1,18 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className="p-10 font-mono">
       <nav className="bg-blue-400 p-5 flex justify-center">
         <Link href="/">
-          <a className="shadow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50">Blog</a>
+          <a className="shadow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50">
+            Blog
+          </a>
         </Link>
         <Link href="/about">
-          <a className="shadow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50">About</a>
+          <a className="shadow md:w-40 bg-blue-50 p-2 m-2 text-center hover:bg-gray-50">
+            About
+          </a>
         </Link>
       </nav>
 
