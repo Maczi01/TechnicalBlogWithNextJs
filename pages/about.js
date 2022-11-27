3d23922ce45d8b6f2999b97a907cd23be01e6f00
MateuszTwardy <mateusz.w.twardy@gmail.com>
2022-11-27 22:23:53 +0000

    Added articles

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,13 @@
 import Layout from 'components/Layout';
+import Head from 'next/head';
 
 export default function About() {
-  return <Layout>About me</Layout>;
+  return (
+    <Layout>
+      <Head>
+        <title>About author</title>
+      </Head>
+      <p>About me</p>
+    </Layout>
+  );
 }
